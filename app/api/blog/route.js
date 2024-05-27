31e48eb7ee6e24aac0c27e910ea3dd1550fbc7c4
nazifishrak <nzfishrak60@gmail.com>
2024-05-26 19:24:54 -0700

    MongoDB connection successful

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -1,6 +1,23 @@
 //this is the route file for the blog api using nextjs routes
 import {NextResponse} from "next/server";
-import Blog from "../../models/blogs/blog.models";
+import Blog from "../../models/blogs/blog.models.js";
 import connectToDatabase from "../../lib/mongodb";
 
 
+// GET
+
+export async function GET(req,res){
+    await connectToDatabase();
+    const blog1 = new Blog({
+        blogTitle: "First Blog",
+        blogContent: "This is the first blog",
+        pictureUrl: "https://www.google.com"
+    });
+    await blog1.save();
+
+
+
+    const blogs = await Blog.findOne({});
+    console.log("Blogs: ", blogs);
+    return NextResponse.json(blogs);
+}
