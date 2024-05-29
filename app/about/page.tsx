58b21048333dd74314ecec949e1b3462d0f1f320
nazifishrak <nzfishrak60@gmail.com>
2024-05-29 14:17:35 -0700

    Contact --> About

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className={`flex flex-row justify-center`}>
+
+            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white"> <mark
+                className="px-2 text-white bg-pink-500 rounded dark:bg-pink-500">Coming</mark> Soon
+            </h1>
+
+
+        </div>
+    );
+};
+
+export default About;
