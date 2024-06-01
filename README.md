ca0bb048b9fa456ec22b6ebf6432aefe41c41ec9
nazifishrak <nzfishrak60@gmail.com>
2024-05-31 20:51:40 -0700

    minor ui changes and update readme link

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Preview
-## [Live Demo ǂ](https://portfolio-website-woad-eta.vercel.app/)
+## [Live Demo ǂ](https:nazif-ishrak.me/)
 ![img.png](img.png)
 ![img_1.png](img_1.png)
 
--- app/components/Blog/BlogItem.tsx
+++ app/components/Blog/BlogItem.tsx
@@ -35,7 +35,6 @@ const BlogItem = ({title, content, imageUrl, slug, category }) => {
                             </Markdown>
 
                         </p>
-                        <br/>
                         <span
                             className="bg-pink-100 text-pink-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">{category}
                         </span>
