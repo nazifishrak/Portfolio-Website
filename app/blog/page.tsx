c6ce93fd34376b40e550059f93592d2c1e71f656
nazifishrak <nzfishrak60@gmail.com>
2024-06-10 14:41:44 -0700

    Fixed blog filter mobile responsiveness

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -35,7 +35,7 @@ export default function Blog() {
 
             <div className="flex pr-10 justify-end">
                 <select
-                    className="w-1/12  bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5  dark:border-gray-600  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className=" lg:w-1/12  bg-transparent border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5  dark:border-gray-600  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     name="category"
                     id="category"
                     value={categoryFilter}
